4adc9d6ed3231373dc92032dd875ce1db2645399
Kavyashreer22 <156086959+Kavyashreer22@users.noreply.github.com>
2024-08-25 15:58:48 +0530

    Update strikeratecalculator.py

--- strikeratecalculator.py
+++ strikeratecalculator.py
@@ -1,12 +1,22 @@
 #Batting Strike rate calculator
 #Runs per 100 balls i.e. it is an average per 100 balls
-print("Welcome to the Batting Strike rate calculator created by KavyaShree R")
-name=input("Please enter the name of the batsman:")
-r = input("Please enter the runs scored by the batsman: ")
-b = input("Please enter the number of balls played by the batsman: ")
-r = int(r)
-b = int(b)
-avg=r/b
-sr=avg*100
-print("The strike rate of the batsman is", sr)
+# Print a welcome message for the user
+  print("Welcome to the Batting Strike rate calculator created by KavyaShree R")
+
+# Prompt the user to enter the name of the batsman
+  name=input("Please enter the name of the batsman:")
+
+# Prompt the user to enter the runs scored by the batsman
+  r = input("Please enter the runs scored by the batsman: ")
+# Prompt the user to enter the number of balls played by the batsman
+  b = input("Please enter the number of balls played by the batsman: ")
+# Convert the runs and balls input from string to integer
+  r = int(r)
+  b = int(b)
+# Calculate the average runs per ball
+  avg=r/b
+# Calculate the strike rate by multiplying the average by 100
+  sr=avg*100
+# Print the strike rate of the batsman
+  print("The strike rate of the batsman is", sr)
 
